667c998425569657c418b3e4c9e17dc459ae33b6
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-07-20 16:59:39 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,27 @@
+
+
+class Author
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def posts
+    Post.all.select{|post| post.author_name == @name}
+  end
+
+  def add_post(post)
+    post.author = self
+  end
+
+  def add_post_by_name(name)
+    post = Post.new(name)
+    self.add_post(post)
+  end
+
+  def self.post_count
+    Post.all.count
+  end
+
+end
