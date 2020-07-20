5deede25ffd09ac4cef96fa3a1d3db17df5217b0
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-07-20 16:49:51 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,7 +2,6 @@ require "pry"
 
 class Artist
   attr_accessor :name
-  @songs = []
 
   def initialize(name)
     @name = name
@@ -12,5 +11,5 @@ class Artist
     song.artist = self
     @songs << song
   end
-  
+
 end
